fa553ee8fb68d4e547b57157e22def1dca600c1f
felipeforte <64996043+felipeforte@users.noreply.github.com>
2023-10-30 14:14:15 -0300

    Update PilhaDinamica.java

--- src/PilhaDinamica.java
+++ src/PilhaDinamica.java
@@ -1,20 +1,24 @@
 public class PilhaDinamica {
     private No topo;
+    private int tamanho;
 
     public PilhaDinamica() {
         topo = null;
+        tamanho = 0;
     }
 
     public void push(Object item) {
         No novo = new No(item);
         novo.proximo = topo;
         topo = novo;
+        tamanho++;
     }
 
     public Object pop() {
         if (topo != null) {
             No item = topo;
             topo = topo.proximo;
+            tamanho--;
             return item.valor;
         }
 
@@ -22,7 +26,21 @@ public class PilhaDinamica {
     }
 
     public Object peek() {
-        return topo.valor;
+        if (topo != null) {
+            return topo.valor;
+        }
+        return null;
+    }
+
+    public int tamanho() {
+        return tamanho;
+    }
+
+    public boolean vazio() {
+        if (tamanho == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
